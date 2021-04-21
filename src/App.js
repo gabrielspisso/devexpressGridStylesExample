c9aa4336134f967dd6e4c2f9befde84510dedba0
gabrielspisso <gabrielspisso@outlook.com>
2021-04-21 20:24:26 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React from "react";
+
+import DataGrid, {
+  Column,
+  Grouping,
+  GroupPanel,
+  Paging,
+  SearchPanel
+} from "devextreme-react/data-grid";
+import CheckBox from "devextreme-react/check-box";
+import { customers } from "./data.js";
+import "./styles.css";
+
+const GroupCell = (props) => {
+  // console.log(props);
+  // return <div>{"pipo"}</div>;
+  return <div style={{ root: { padding: 0 } }}> </div>;
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.grid = React.createRef();
+
+    this.state = {
+      autoExpandAll: true
+    };
+
+    this.onAutoExpandAllChanged = this.onAutoExpandAllChanged.bind(this);
+  }
+
+  render() {
+    const defaultColumnProps = {};
+    return (
+      <div>
+        <DataGrid
+          ref={this.grid}
+          dataSource={customers}
+          allowColumnReordering={true}
+          showBorders={true}
+          onRowPrepared={(e) => {
+            if (e.rowType == "group") {
+              e.rowElement.style.height = "0";
+            }
+            console.log(e);
+            e.rowElement.style.padding = "0";
+          }}
+          onCellPrepared={(e) => {
+            //console.log("e", e);
+            if (e.rowType == "group") {
+              e.cellElement.style.padding = "0";
+            }
+          }}
+        >
+          <GroupPanel visible={true} />
+          <SearchPanel visible={true} />
+          <Grouping
+            autoExpandAll={this.state.autoExpandAll}
+            allowCollapsing={false}
+          />
+          <Paging defaultPageSize={10} />
+
+          <Column dataField="CompanyName" dataType="string" />
+          <Column dataField="Phone" dataType="string" />
+          <Column dataField="Fax" dataType="string" />
+          <Column dataField="City" dataType="string" />
+          <Column
+            dataField="State"
+            dataType="string"
+            groupIndex={0}
+            allowGrouping={false}
+            groupCellComponent={GroupCell}
+            headerCellComponent={GroupCell}
+          />
+        </DataGrid>
+
+        <div className="options">
+          <div className="caption">Options</div>
+          <div className="option">
+            <CheckBox
+              text="Expand All Groups"
+              value={this.state.autoExpandAll}
+              onValueChanged={this.onAutoExpandAllChanged}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  onAutoExpandAllChanged() {
+    this.setState({
+      autoExpandAll: !this.state.autoExpandAll
+    });
+  }
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const customers = [{
+  'ID' : 1,
+  'CompanyName' : 'Premier Buy',
+  'Address' : '7601 Penn Avenue South',
+  'City' : 'Richfield',
+  'State' : 'Minnesota',
+  'Zipcode' : 55423,
+  'Phone' : '(612) 291-1000',
+  'Fax' : '(612) 291-2001',
+  'Website' : 'http =//www.nowebsitepremierbuy.com'
+}, {
+  'ID' : 2,
+  'CompanyName' : 'ElectrixMax',
+  'Address' : '263 Shuman Blvd',
+  'City' : 'Naperville',
+  'State' : 'Illinois',
+  'Zipcode' : 60563,
+  'Phone' : '(630) 438-7800',
+  'Fax' : '(630) 438-7801',
+  'Website' : 'http =//www.nowebsiteelectrixmax.com'
+}, {
+  'ID' : 3,
+  'CompanyName' : 'Video Emporium',
+  'Address' : '1201 Elm Street',
+  'City' : 'Dallas',
+  'Zipcode' : 75270,
+  'Phone' : '(214) 854-3000',
+  'Fax' : '(214) 854-3001',
+  'Website' : 'http =//www.nowebsitevideoemporium.com'
+}];
